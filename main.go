67e4afde0235e8eabdcc20a0f6a64659dc3f1cc0
Simple-Tracker <admin@hloli.org>
2023-06-11 19:11:32 +0800

    Add log file support

--- main.go
+++ main.go
@@ -37,10 +37,12 @@ type TorrentPeersStruct struct {
 }
 type ConfigStruct struct {
 	Debug bool
+	LogToFile bool
 	QBURL string
 	BlockList []string
 }
 
+var todayStr = ""
 var currentTimestamp int64 = 0
 var lastMaindataRid int64 = 0
 var lastTorrentPeersRid int64 = 0
@@ -49,48 +51,80 @@ var blockListCompiled []*regexp.Regexp
 var httpClient = http.Client {
     Timeout: 8 * time.Second,
 }
-var config = ConfigStruct{ Debug: false, QBURL: "http://127.0.0.1:990", BlockList: []string {} }
+var config = ConfigStruct{ Debug: false, LogToFile: true, QBURL: "http://127.0.0.1:990", BlockList: []string {} }
 var configFilename = "config.json"
 var configLastMod int64 = 0;
+var logFile *os.File
 
-func Log(module string, str string, args ...interface{}) {
+func Log(module string, str string, logToFile bool, args ...interface{}) {
 	if !config.Debug && strings.HasPrefix(module, "Debug") {
 		return
 	}
 	logStr := fmt.Sprintf("[" + module + "] " + str + ".\n", args...)
+	if logToFile && config.LogToFile && logFile != nil {
+		if _, err := logFile.Write([]byte(logStr)); err != nil {
+			Log("Log", "无法写入日志", false)
+		}
+	}
 	fmt.Print(logStr)
+}
+func GetDateTime(withTime bool) string {
+	formatStr := "2006-01-02"
+	if withTime {
+		formatStr += " 15:04:05"
+	}
+	return time.Now().Format(formatStr)
+}
+func ReloadLog() {
+	tmpTodayStr := GetDateTime(false)
+	if todayStr != tmpTodayStr {
+		todayStr = tmpTodayStr
+		logFile.Close()
+
+		tLogFile, err := os.OpenFile("logs/" + todayStr + ".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			tLogFile.Close()
+			tLogFile = nil
+			Log("ReloadLog", "无法访问日志", false)
+		}
+		logFile = tLogFile
+	}
 }
 func LoadConfig() bool {
 	configFileStat, err := os.Stat(configFilename)
 	if err != nil {
-		Log("Debug-LoadConfig", "读取配置文件元数据时发生了错误: " + err.Error())
+		Log("Debug-LoadConfig", "读取配置文件元数据时发生了错误: " + err.Error(), false)
 	}
 	tmpConfigLastMod := configFileStat.ModTime().Unix()
 	if tmpConfigLastMod <= configLastMod {
 		return true
 	}
 	if configLastMod != 0 {
-		Log("Debug-LoadConfig", "发现配置文件更改, 正在进行热重载")
+		Log("Debug-LoadConfig", "发现配置文件更改, 正在进行热重载", false)
 	}
-	configLastMod = tmpConfigLastMod
 	configFile, err := ioutil.ReadFile(configFilename)
 	if err != nil {
-		Log("LoadConfig", "读取配置文件时发生了错误: " + err.Error())
+		Log("LoadConfig", "读取配置文件时发生了错误: " + err.Error(), false)
 		return false
 	}
+	configLastMod = tmpConfigLastMod
 	json.Unmarshal(configFile, &config)
-	Log("LoadConfig", "读取配置文件成功")
+	if config.LogToFile {
+		os.Mkdir("logs", os.ModePerm)
+		ReloadLog()
+	}
+	Log("LoadConfig", "读取配置文件成功", true)
 	t := reflect.TypeOf(config)
 	v := reflect.ValueOf(config)
 	for k := 0; k < t.NumField(); k++ {
-		Log("LoadConfig-Current", "%v: %v", t.Field(k).Name, v.Field(k).Interface())
+		Log("LoadConfig-Current", "%v: %v", true, t.Field(k).Name, v.Field(k).Interface())
 	}
 	blockListCompiled = make([]*regexp.Regexp, len(config.BlockList))
 	for k, v := range config.BlockList {
-		Log("Debug-LoadConfig-CompileBlockList", "%s", v)
+		Log("Debug-LoadConfig-CompileBlockList", "%s", false, v)
 		reg, err := regexp.Compile("(?i)" + v)
 		if err != nil {
-			Log("LoadConfig-CompileBlockList", "表达式 %s 有错误", v)
+			Log("LoadConfig-CompileBlockList", "表达式 %s 有错误", true, v)
 			continue
 		}
 		blockListCompiled[k] = reg
@@ -123,14 +157,14 @@ func GenBlockPeersStr() string {
 func Fetch(url string) []byte {
 	response, err := httpClient.Get(url)
 	if err != nil {
-		Log("Fetch", "请求时发生了错误: " + err.Error())
+		Log("Fetch", "请求时发生了错误: " + err.Error(), false)
 		return nil
 	}
 	defer response.Body.Close()
 
 	responseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		Log("Fetch", "读取时发生了错误")
+		Log("Fetch", "读取时发生了错误", false)
 		return nil
 	}
 
@@ -139,14 +173,14 @@ func Fetch(url string) []byte {
 func Submit(url string, postdata string) []byte {
 	response, err := httpClient.Post(url, "application/x-www-form-urlencoded", strings.NewReader(postdata))
 	if err != nil {
-		Log("Submit", "请求时发生了错误: " + err.Error())
+		Log("Submit", "请求时发生了错误: " + err.Error(), false)
 		return nil
 	}
 	defer response.Body.Close()
 
 	responseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		Log("Submit", "读取时发生了错误")
+		Log("Submit", "读取时发生了错误", false)
 		return nil
 	}
 
@@ -155,36 +189,36 @@ func Submit(url string, postdata string) []byte {
 func FetchMaindata(rid int64) *MainDataStruct {
 	maindataResponseBody := Fetch(config.QBURL + "/api/v2/sync/maindata?rid=" + strconv.FormatInt(rid, 10))
 	if maindataResponseBody == nil {
-		Log("FetchMaindata", "发生错误")
+		Log("FetchMaindata", "发生错误", false)
 		return nil
 	}
 	
 	var mainDataResult MainDataStruct
 	if err := json.Unmarshal(maindataResponseBody, &mainDataResult); err != nil {
-		Log("FetchMaindata", "解析时发生了错误")
+		Log("FetchMaindata", "解析时发生了错误", false)
 		return nil
 	}
 
 	lastMaindataRid = mainDataResult.Rid
-	Log("Debug-FetchMaindata", fmt.Sprintf("rid: %d, 完整更新: %s", lastMaindataRid, strconv.FormatBool(mainDataResult.FullUpdate)))
+	Log("Debug-FetchMaindata", "rid: %d, 完整更新: %s", false, lastMaindataRid, strconv.FormatBool(mainDataResult.FullUpdate))
 
 	return &mainDataResult
 }
 func FetchTorrentPeers(rid int64, infoHash string) *TorrentPeersStruct {
 	torrentPeersResponseBody := Fetch(config.QBURL + "/api/v2/sync/torrentPeers?rid=" + strconv.FormatInt(rid, 10) + "&hash=" + infoHash)
 	if torrentPeersResponseBody == nil {
-		Log("FetchTorrentPeers", "发生错误")
+		Log("FetchTorrentPeers", "发生错误", false)
 		return nil
 	}
 
 	var torrentPeersResult TorrentPeersStruct
 	if err := json.Unmarshal(torrentPeersResponseBody, &torrentPeersResult); err != nil {
-		Log("FetchTorrentPeers", "解析时发生了错误")
+		Log("FetchTorrentPeers", "解析时发生了错误", false)
 		return nil
 	}
 
 	lastTorrentPeersRid = torrentPeersResult.Rid
-	Log("Debug-FetchTorrentPeers", fmt.Sprintf("rid: %d, 完整更新: %s", lastTorrentPeersRid, strconv.FormatBool(torrentPeersResult.FullUpdate)))
+	Log("Debug-FetchTorrentPeers", "rid: %d, 完整更新: %s", false, lastTorrentPeersRid, strconv.FormatBool(torrentPeersResult.FullUpdate))
 
 	return &torrentPeersResult
 }
@@ -192,7 +226,7 @@ func SubmitBlockPeers(banIPsStr string) {
 	banIPsStr = url.QueryEscape("{\"banned_IPs\": \"" + banIPsStr + "\"}")
 	banResponseBody := Submit(config.QBURL + "/api/v2/app/setPreferences", "json=" + banIPsStr)
 	if banResponseBody == nil {
-		Log("SubmitBlockPeers", "发生错误")
+		Log("SubmitBlockPeers", "发生错误", false)
 	}
 }
 func Task() {
@@ -204,7 +238,7 @@ func Task() {
 		}
 	}
 	if cleanCount != 0 {
-		Log("Task", "已清理过期客户端: %d 个", cleanCount)
+		Log("Task", "已清理过期客户端: %d 个", true, cleanCount)
 	}
 
 	metadata := FetchMaindata(lastMaindataRid)
@@ -214,7 +248,7 @@ func Task() {
 
 	blockCount := 0
 	for infoHash, _ := range metadata.Torrents {
-		Log("Debug-Task_CheckHash", "%s", infoHash)
+		Log("Debug-Task_CheckHash", "%s", false, infoHash)
 		if infoHash == "" {
 			continue
 		}
@@ -224,14 +258,14 @@ func Task() {
 				continue
 			}
 			if IsBlockedPeer(peerInfo.IP, true) {
-				Log("Debug-Task_IgnorePeer", "%s %s", peerInfo.IP, peerInfo.Client)
+				Log("Debug-Task_IgnorePeer", "%s %s", false, peerInfo.IP, peerInfo.Client)
 				continue
 			}
-			Log("Debug-Task_CheckPeer", "%s %s", peerInfo.IP, peerInfo.Client)
+			Log("Debug-Task_CheckPeer", "%s %s", false, peerInfo.IP, peerInfo.Client)
 			for _, v := range blockListCompiled {
 				if v.MatchString(peerInfo.Client) {
 					blockCount++
-					Log("Task_AddBlockPeer", "%s %s", peerInfo.IP, peerInfo.Client)
+					Log("Task_AddBlockPeer", "%s %s", true, peerInfo.IP, peerInfo.Client)
 					AddBlockPeer(peerInfo.IP, peerInfo.Client)
 					break
 				}
@@ -240,13 +274,14 @@ func Task() {
 	}
 	if cleanCount != 0 || blockCount != 0 {
 		peersStr := GenBlockPeersStr()
-		Log("Debug-Task_GenBlockPeersStr", "%s", peersStr)
+		Log("Debug-Task_GenBlockPeersStr", "%s", false, peersStr)
 		SubmitBlockPeers(peersStr)
-		Log("Task", "此次封禁客户端: %d 个, 当前封禁客户端: %d 个", blockCount, len(blockPeerMap))
+		Log("Task", "此次封禁客户端: %d 个, 当前封禁客户端: %d 个", true, blockCount, len(blockPeerMap))
 	}
 }
 func main() {
-	Log("Main", "程序已启动")
+	LoadConfig()
+	Log("Main", "程序已启动", true)
 	for range time.Tick(2 * time.Second) {
 		currentTimestamp = time.Now().Unix()
 		LoadConfig()
