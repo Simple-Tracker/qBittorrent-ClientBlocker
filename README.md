015990ad6ae040b3c1d93bba07a27c1e28fc6485
Simple-Tracker <admin@hloli.org>
2023-06-11 14:51:37 +0800

    Sync change

---
+++ README.md
@@ -0,0 +1,7 @@
+# qBittorrent-ClientBlocker
+一款适用于 qBittorrent 的客户端屏蔽器. (开发尚未完成)
+
+预期目标:
+* 支持热重载配置
+* 用户可自定义 Ban List
+* 支持忽略私有 IP 地址
--- main.go
+++ main.go
@@ -3,37 +3,53 @@ package main
 import (
 	"os"
 	"fmt"
+	"net"
 	"time"
+	"regexp"
 	"strings"
 	"strconv"
 	"reflect"
+	"net/url"
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
 )
 
-type BlockClientInfoStruct struct {
+type BlockPeerInfoStruct struct {
 	Timestamp int64
-	PeerID string
-}
-type TorrentStruct struct {
+	Name string
 }
 type MainDataStruct struct {
 	Rid int64 `json:"rid"`
 	FullUpdate bool `json:"full_update"`
 	Torrents map[string]TorrentStruct `json:"torrents"`
 }
+type TorrentStruct struct {
+}
+type PeerStruct struct {
+	IP string
+	Client string
+}
+type TorrentPeersStruct struct {
+	Rid int64 `json:"rid"`
+	FullUpdate bool `json:"full_update"`
+	Peers map[string]PeerStruct `json:"peers"`
+}
 type ConfigStruct struct {
 	Debug bool
 	QBURL string
+	BlockList []string
 }
 
-var blockClientMap = make(map[string]BlockClientInfoStruct)
+var currentTimestamp int64 = 0
 var lastMaindataRid int64 = 0
+var lastTorrentPeersRid int64 = 0
+var blockPeerMap = make(map[string]BlockPeerInfoStruct)
+var blockListCompiled []*regexp.Regexp
 var httpClient = http.Client {
     Timeout: 8 * time.Second,
 }
-var config = ConfigStruct{ Debug: false, QBURL: "http://127.0.0.1:990" }
+var config = ConfigStruct{ Debug: false, QBURL: "http://127.0.0.1:990", BlockList: []string {} }
 var configFilename = "config.json"
 var configLastMod int64 = 0;
 
@@ -69,39 +85,122 @@ func LoadConfig() bool {
 	for k := 0; k < t.NumField(); k++ {
 		Log("LoadConfig-Current", "%v: %v", t.Field(k).Name, v.Field(k).Interface())
 	}
+	blockListCompiled = make([]*regexp.Regexp, len(config.BlockList))
+	for k, v := range config.BlockList {
+		Log("Debug-LoadConfig-CompileBlockList", "%s", v)
+		reg, err := regexp.Compile("(?i)" + v)
+		if err != nil {
+			Log("LoadConfig-CompileBlockList", "表达式 %s 有错误", v)
+			continue
+		}
+		blockListCompiled[k] = reg
+	}
 	return true
 }
-func AddBlockClient(clientIP string, clientPeerID string) {
-	blockClientMap[strings.ToLower(clientIP)] = BlockClientInfoStruct{ Timestamp: time.Now().Unix(), PeerID: clientPeerID }
+func CheckPrivateIP(ip string) bool {
+    ipParsed := net.ParseIP(ip)
+    return ipParsed.IsPrivate()
+}
+func AddBlockPeer(clientIP string, clientName string) {
+	blockPeerMap[strings.ToLower(clientIP)] = BlockPeerInfoStruct{ Timestamp: currentTimestamp, Name: clientName }
+}
+func IsBlockedPeer(clientIP string, updateTimestamp bool) bool {
+	if blockPeer, exist := blockPeerMap[clientIP]; exist {
+		if updateTimestamp {
+			blockPeer.Timestamp = currentTimestamp
+		}
+		return true
+	}
+	return false
+}
+func GenBlockPeersStr() string {
+	ips := ""
+	for k := range blockPeerMap {
+		ips += k + "\n"
+	}
+	return ips
+}
+func Fetch(url string) []byte {
+	response, err := httpClient.Get(url)
+	if err != nil {
+		Log("Fetch", "请求时发生了错误: " + err.Error())
+		return nil
+	}
+	defer response.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		Log("Fetch", "读取时发生了错误")
+		return nil
+	}
+
+	return responseBody
+}
+func Submit(url string, postdata string) []byte {
+	response, err := httpClient.Post(url, "application/x-www-form-urlencoded", strings.NewReader(postdata))
+	if err != nil {
+		Log("Submit", "请求时发生了错误: " + err.Error())
+		return nil
+	}
+	defer response.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		Log("Submit", "读取时发生了错误")
+		return nil
+	}
+
+	return responseBody
 }
 func FetchMaindata(rid int64) *MainDataStruct {
-	maindataResponse, err := httpClient.Get(config.QBURL + "/api/v2/sync/maindata?rid=" + strconv.FormatInt(rid, 10))
-	if err != nil {
-		Log("Maindata", "请求时发生了错误: " + err.Error())
+	maindataResponseBody := Fetch(config.QBURL + "/api/v2/sync/maindata?rid=" + strconv.FormatInt(rid, 10))
+	if maindataResponseBody == nil {
+		Log("FetchMaindata", "发生错误")
 		return nil
 	}
-	defer maindataResponse.Body.Close()
-	maindataResponseBody, err := ioutil.ReadAll(maindataResponse.Body)
-
+	
 	var mainDataResult MainDataStruct
 	if err := json.Unmarshal(maindataResponseBody, &mainDataResult); err != nil {
-		Log("Maindata", "解析时发生了错误")
+		Log("FetchMaindata", "解析时发生了错误")
 		return nil
 	}
+
 	lastMaindataRid = mainDataResult.Rid
-	Log("Debug-Maindata", fmt.Sprintf("rid: %d, 完整更新: %s", lastMaindataRid, strconv.FormatBool(mainDataResult.FullUpdate)))
+	Log("Debug-FetchMaindata", fmt.Sprintf("rid: %d, 完整更新: %s", lastMaindataRid, strconv.FormatBool(mainDataResult.FullUpdate)))
 
 	return &mainDataResult
 }
-func FetchTorrentPeers(rid int64, infoHash string) string {
-	return ""
+func FetchTorrentPeers(rid int64, infoHash string) *TorrentPeersStruct {
+	torrentPeersResponseBody := Fetch(config.QBURL + "/api/v2/sync/torrentPeers?rid=" + strconv.FormatInt(rid, 10) + "&hash=" + infoHash)
+	if torrentPeersResponseBody == nil {
+		Log("FetchTorrentPeers", "发生错误")
+		return nil
+	}
+
+	var torrentPeersResult TorrentPeersStruct
+	if err := json.Unmarshal(torrentPeersResponseBody, &torrentPeersResult); err != nil {
+		Log("FetchTorrentPeers", "解析时发生了错误")
+		return nil
+	}
+
+	lastTorrentPeersRid = torrentPeersResult.Rid
+	Log("Debug-FetchTorrentPeers", fmt.Sprintf("rid: %d, 完整更新: %s", lastTorrentPeersRid, strconv.FormatBool(torrentPeersResult.FullUpdate)))
+
+	return &torrentPeersResult
+}
+func SubmitBlockPeers(banIPsStr string) {
+	banIPsStr = url.QueryEscape("{\"banned_IPs\": \"" + banIPsStr + "\"}")
+	banResponseBody := Submit(config.QBURL + "/api/v2/app/setPreferences", "json=" + banIPsStr)
+	if banResponseBody == nil {
+		Log("SubmitBlockPeers", "发生错误")
+	}
 }
 func Task() {
 	cleanCount := 0
-	for clientIP, clientInfo := range blockClientMap {
-		if clientInfo.Timestamp + 86400 < time.Now().Unix() {
+	for clientIP, clientInfo := range blockPeerMap {
+		if clientInfo.Timestamp + 86400 < currentTimestamp {
 			cleanCount++
-			delete(blockClientMap, clientIP)
+			delete(blockPeerMap, clientIP)
 		}
 	}
 	if cleanCount != 0 {
@@ -112,17 +211,44 @@ func Task() {
 	if metadata == nil {
 		return
 	}
-	//var lastTorrentRid int64 = 0
+
+	blockCount := 0
 	for infoHash, _ := range metadata.Torrents {
-		Log("Debug-Task", "%s", infoHash)
-		if infoHash != "" {
-			//torrentPeers := FetchTorrentPeers(lastTorrentRid, infoHash)
+		Log("Debug-Task_CheckHash", "%s", infoHash)
+		if infoHash == "" {
+			continue
 		}
+		torrentPeers := FetchTorrentPeers(lastTorrentPeersRid, infoHash)
+		for _, peerInfo := range torrentPeers.Peers {
+			if peerInfo.IP == "" || peerInfo.Client == "" || CheckPrivateIP(peerInfo.IP) {
+				continue
+			}
+			if IsBlockedPeer(peerInfo.IP, true) {
+				Log("Debug-Task_IgnorePeer", "%s %s", peerInfo.IP, peerInfo.Client)
+				continue
+			}
+			Log("Debug-Task_CheckPeer", "%s %s", peerInfo.IP, peerInfo.Client)
+			for _, v := range blockListCompiled {
+				if v.MatchString(peerInfo.Client) {
+					blockCount++
+					Log("Task_AddBlockPeer", "%s %s", peerInfo.IP, peerInfo.Client)
+					AddBlockPeer(peerInfo.IP, peerInfo.Client)
+					break
+				}
+			}
+		}
+	}
+	if cleanCount != 0 || blockCount != 0 {
+		peersStr := GenBlockPeersStr()
+		Log("Debug-Task_GenBlockPeersStr", "%s", peersStr)
+		SubmitBlockPeers(peersStr)
+		Log("Task", "此次封禁客户端: %d 个, 当前封禁客户端: %d 个", blockCount, len(blockPeerMap))
 	}
 }
 func main() {
 	Log("Main", "程序已启动")
 	for range time.Tick(2 * time.Second) {
+		currentTimestamp = time.Now().Unix()
 		LoadConfig()
 		Task()
 	}
