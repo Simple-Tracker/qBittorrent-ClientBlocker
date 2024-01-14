570914fe29f87873b1d1e7eb3367b3429631e971
Simple-Tracker <admin@hloli.org>
2024-01-14 20:04:52 +0800

    Add hotkey support for Windows

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//go:build !windows
 package main
 
 func main() {
---
+++ main_windows.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/lxn/win"
+	"golang.design/x/hotkey"
+)
+
+var showWindow = true
+
+func ShowOrHiddenWindow() {
+	consoleWindow := win.GetConsoleWindow()
+	if showWindow == true {
+		Log("Debug-ShowOrHiddenWindow", "窗口隐藏", false)
+		showWindow = false
+		win.ShowWindow(consoleWindow, win.SW_HIDE)
+	} else {
+		Log("Debug-ShowOrHiddenWindow", "窗口显示", false)
+		showWindow = true
+		win.ShowWindow(consoleWindow, win.SW_SHOW)
+	}
+}
+func RegHotKey() {
+	qBCBHotkey := hotkey.New([]hotkey.Modifier { hotkey.ModCtrl, hotkey.ModAlt }, hotkey.KeyB)
+	err := qBCBHotkey.Register()
+	if err != nil {
+		Log("RegHotKey", "无法注册窗口热键: %v", false, err)
+		return
+	}
+	Log("RegHotKey", "已注册窗口热键: CTRL+ALT+B", false)
+
+	for range qBCBHotkey.Keydown() {
+		Log("Debug-RegHotKey", "开始监听窗口热键", false)
+		ShowOrHiddenWindow()
+	}
+}
+func main() {
+	go RegHotKey()
+	RunConsole()
+}
