1c1927d9e1389c409c580ff7580a8e9760f31518
Simple-Tracker <admin@hloli.org>
2024-02-05 01:26:42 +0800

    Bug fix & Improve

--- request.go
+++ request.go
@@ -11,24 +11,21 @@ func Fetch(url string, tryLogin bool) []byte {
 		Log("Fetch", "请求时发生了错误: %s", true, err.Error())
 		return nil
 	}
-	if response.StatusCode == 403 && (!tryLogin || !Login()) {
-		Log("Fetch", "请求时发生了错误: 认证失败", true)
-		return nil
-	}
-	if response.StatusCode == 404 {
-		Log("Fetch", "请求时发生了错误: 资源不存在", true)
-		return nil
-	}
-	response, err = httpClient.Get(url)
-	if err != nil {
-		Log("Fetch", "请求时发生了错误: %s", true, err.Error())
-		return nil
-	}
+	responseBody, err := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
 
-	responseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		Log("Fetch", "读取时发生了错误: %s", true, err.Error())
+		return nil
+	}
+
+	if response.StatusCode == 403 && (!tryLogin || !Login()) {
+		Log("Fetch", "请求时发生了错误: 认证失败 %s", true, responseBody)
+		return nil
+	}
+
+	if response.StatusCode == 404 {
+		Log("Fetch", "请求时发生了错误: 资源不存在", true)
 		return nil
 	}
 
@@ -40,20 +37,21 @@ func Submit(url string, postdata string, tryLogin bool) []byte {
 		Log("Submit", "请求时发生了错误: %s", true, err.Error())
 		return nil
 	}
-	if response.StatusCode == 403 && (!tryLogin || !Login()) {
-		Log("Submit", "请求时发生了错误: 认证失败", true)
-		return nil
-	}
-	response, err = httpClient.Post(url, "application/x-www-form-urlencoded", strings.NewReader(postdata))
-	if err != nil {
-		Log("Submit", "请求时发生了错误: %s", true, err.Error())
-		return nil
-	}
+	responseBody, err := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
 
-	responseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		Log("Submit", "读取时发生了错误", true)
+		Log("Submit", "读取时发生了错误: %s", true, err.Error())
+		return nil
+	}
+
+	if response.StatusCode == 403 && (!tryLogin || !Login()) {
+		Log("Submit", "请求时发生了错误: 认证失败 %s", true, responseBody)
+		return nil
+	}
+
+	if response.StatusCode == 404 {
+		Log("Submit", "请求时发生了错误: 资源不存在", true)
 		return nil
 	}
 
