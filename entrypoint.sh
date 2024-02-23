1a2a238e4df87bb0e27ec756e19643e90d772c1d
Simple-Tracker <admin@hloli.org>
2024-02-24 05:04:34 +0800

    Add IPBlockList support to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,25 @@ else
     echo "config.json not exist, generate config from env"
 
     # Convert $blockList to json array
+    tmpBlockList="[]"
+    tmpIPBlockList="[]"
     if [ -z "$blockList" ]; then
-        tmpBlockList=$(jq -n --argjson arr "[]" '$arr')
-    else
         tmpBlockList=$(echo $blockList | jq '.')
     fi
+    if [ -z "$ipBlockList" ]; then
+        tmpIPBlockList=$(echo $ipBlockList | jq '.')
+    else
 
     envKVPair=$(jq -n 'env|to_entries[]')
 
     # Keep username and password string
     # Keep blockList json array
     # Convert "true" to true, "false" to false, digital string to number
-    configKVPair=$(echo $envKVPair | jq --argjson tempBlockList "$tmpBlockList" '{
+    configKVPair=$(echo $envKVPair | jq --argjson tmpBlockList "$tmpBlockList" --argjson tmpIPBlockList "$tmpIPBlockList" '{
         (.key): (
             if (.key|ascii_downcase) == "qbusername" or (.key|ascii_downcase) == "qbpassword" then .value
-            elif (.key|ascii_downcase) == "blocklist" then $tempBlockList
+            elif (.key|ascii_downcase) == "blocklist" then $tmpBlockList
+            elif (.key|ascii_downcase) == "ipblocklist" then $tmpIPBlockList
             else .value|(
                 if . == "true" then true
                 elif . == "false" then false
