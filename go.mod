517983a82acb909d99f038c8117c5ae45f183950
Simple-Tracker <admin@hloli.org>
2024-05-04 14:34:07 +0800

    Add SysTray

--- go.mod
+++ go.mod
@@ -6,7 +6,16 @@ require (
 	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/Xuanwo/go-locale v1.1.0 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/getlantern/systray v1.2.2 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/tidwall/jsonc v0.3.2 // indirect
 	golang.design/x/hotkey v0.4.1 // indirect
 	golang.org/x/net v0.21.0 // indirect
--- main_windows.go
+++ main_windows.go
@@ -4,6 +4,7 @@ package main
 import (
 	"github.com/lxn/win"
 	"golang.design/x/hotkey"
+	"github.com/getlantern/systray"
 )
 
 var showWindow = true
@@ -11,7 +12,7 @@ var qBCBHotkey = hotkey.New([]hotkey.Modifier { hotkey.ModCtrl, hotkey.ModAlt },
 
 func ShowOrHiddenWindow() {
 	consoleWindow := win.GetConsoleWindow()
-	if showWindow == true {
+	if showWindow {
 		Log("Debug-ShowOrHiddenWindow", GetLangText("Debug-ShowOrHiddenWindow_HideWindow"), false)
 		showWindow = false
 		win.ShowWindow(consoleWindow, win.SW_HIDE)
@@ -33,12 +34,33 @@ func RegHotKey() {
 		ShowOrHiddenWindow()
 	}
 }
+func RegSysTray() {
+	systray.Run(func () {
+		systray.SetIcon(icon_Windows)
+		systray.SetTitle(programName)
+		mShow := systray.AddMenuItem("显示/隐藏", "显示/隐藏程序")
+		mQuit := systray.AddMenuItem("退出", "退出程序")
+
+		for {
+			select {
+				case <-mShow.ClickedCh:
+					ShowOrHiddenWindow()
+				case <-mQuit.ClickedCh:
+					systray.Quit()
+			}
+		}
+	}, func () {
+		ReqStop()
+	})
+}
 func main() {
 	if PrepareEnv() {
 		go RegHotKey()
+		go RegSysTray()
 		RunConsole()
 	}
 }
 func Platform_Stop() {
 	qBCBHotkey.Unregister()
+	systray.Quit()
 }
