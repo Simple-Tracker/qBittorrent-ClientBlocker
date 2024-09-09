dc347a33870d3c794aadadaf1710cd4ab02d5686
Yusheng <amenomorikou@gmail.com>
2024-09-09 11:34:08 +0800

    doc: added
    Signed-off-by: Yusheng <amenomorikou@gmail.com>

--- util.go
+++ util.go
@@ -204,6 +204,11 @@ func ExecCommand(command string) (bool, string, string) {
 
 	return true, string(out), ""
 }
+
+// EraseSyncMap is a helper function to erase all elements in a sync.Map.
+// This function is supposed to be replaced by `Clear` method after Go1.23 and above.
+//
+// go:build !go1.23
 func EraseSyncMap(m *sync.Map) {
 	m.Range(func(key, value any) bool {
 		m.Delete(key)
